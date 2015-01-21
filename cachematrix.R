c43054bc63b42a66a28091bf741df4783acee3ad
ealbeor <alberto.ortega@ericsson.com>
2015-01-21 19:15:11 +0000

    R code implemented for assigment 2 of R programming course
    Change-Id: I23d876a8df994e604f62e9fcfd2b6e20f4e626c6

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions used to calculate the inverse of a matrix, caching to achieve that it is calculated only once.
+## CacheSolve must be invoked with the result of invoking makeCacheMatrix over a matrix.
+##
+## A use example:
+## m <- matrix(c(2,10,20,3), 2 ,2)
+## preparedMatrix <- makeCacheMatrix(m)
+## cacheSolve(preparedMatrix)
+##
+## First invocation to cacheSolve(preparedMatrix) will calculate the inverse of matrix m and cache it, 
+## following invocations will return the cached value
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates a list of functions that will be the input to calculate the inverse of the matrix passed as
+## input parameter
+## Functions returned are:
+##      set --> set the matrix to use in calculations
+##      get --> return the matrix used in calculations
+##      setSolve --> set the cached inverse matrix (but it does not calculate it). 
+##                     It will be used ONLY by cachedSolve function, and by nobody else
+##      getSolve --> return the previously set inverse matrix 
+makeCacheMatrix <- function(matrix = matrix()) {
+  
+    matrixSolve <- NULL
+    set <- function(newMatrix) {
+        matrix <<- newMatrix
+        matrixSolve <<- NULL
+    }
+    get <- function() {
+        matrix 
+    }
+    setSolve <- function(newSolve) { 
+        matrixSolve <<- newSolve 
+    }
+    getSolve <- function() { 
+        matrixSolve 
+    }
+    list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Calculates the inverse of the matrix stored in the list passed as parameter. If it was previously 
+## calculated, it return the stored value without doing any calculation, else it calculates the inverse
+## and caches it
+##
+## The input parameter must be a list created by makeCacheMatrix function, otherwise it will not work
+## as expected
+cacheSolve <- function(preProcessedMatrix, ...) {
+        
+    solvedMatrix <- preProcessedMatrix$getSolve()
+    if(!is.null(solvedMatrix)) {
+        return(solvedMatrix)
+    }
+    rawMatrix <- preProcessedMatrix$get()
+    solvedMatrix <- solve(rawMatrix)
+    preProcessedMatrix$setSolve(solvedMatrix)
+    solvedMatrix
 }
